3eaefc3c9fcb5c90cf02f00aad40198e5dc99e38
gitdevjin <dev.hjshin@gmail.com>
2025-01-31 16:03:17 -0500

    work started

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,6 @@ function App() {
         <h1 className="text-3xl text-green-200 font-bold underline">
           Hello world!
         </h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -28,9 +22,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
